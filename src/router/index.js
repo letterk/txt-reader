63a02f56cc643ab1afdcae8c152614013851ce2a
letterk <kissoul@126.com>
2025-05-30 17:21:41 +0800

    feat(router): 更新路由配置以支持章节索引参数
    - 在 Reader 路由路径中添加可选的章节索引参数

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,3 @@
-// src/router/index.js
-
 import { createRouter, createWebHistory } from 'vue-router'
 
 import BookshelfView from '../views/BookshelfView.vue'
@@ -12,14 +10,9 @@ const routes = [
     component: BookshelfView,
   },
   {
-    // 路径为 '/read/:bookId'
-    // ':bookId' 是一个动态片段（参数），它会匹配 URL 中的一部分，并作为参数传递给组件
-    // 例如，访问 '/read/1'，bookId 就会是 '1'
-    path: '/read/:bookId',
+    path: '/read/:bookId/:chapterIndex?',
     name: 'Reader',
     component: ReaderView,
-    // props: true 表示将路由参数（这里是 bookId）作为组件的 props 传递
-    // ReaderView 组件就可以通过 props 接收 bookId 了
     props: true,
   },
 ]
